01fce3ec4d4d3cbbc8cbe34ca43e4cfc8207c385
Caleb Smith <caleb13360@gmail.com>
2024-12-09 08:15:22 +1100

    added participants table

--- wsrc-app/src/app/race-details/race-details.component.ts
+++ wsrc-app/src/app/race-details/race-details.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-race-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './race-details.component.html',
   styleUrl: './race-details.component.css'
 })
 export class RaceDetailsComponent {
-
+  racers = Array.from({ length: 18 }, (_, i) => ({
+    name: `Racer ${i + 1}`,
+    country: `Country ${i + 1}`,
+    lastCompeted: `${i + 1} days ago`
+}));
 }

5270e83a237278b5e8b146aecdcebd7914c96a7c
Caleb Smith <caleb13360@gmail.com>
2024-12-09 07:29:59 +1100

    Added user profile page

--- wsrc-app/src/app/app.routes.ts
+++ wsrc-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RacesComponent } from './races/races.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const routes: Routes = [
     {
@@ -18,5 +19,10 @@ export const routes: Routes = [
         path: 'about',
         component: AboutUsComponent,
         title: 'About Us'
+    },
+    {
+        path: 'profile',
+        component: UserProfileComponent,
+        title: 'Profile'
     }
 ];

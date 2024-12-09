af1bd686bcf31bb48a575f06f72d6c18a472279c
Caleb Smith <caleb13360@gmail.com>
2024-12-10 09:19:57 +1100

    added new pages

--- wsrc-app/src/app/app.routes.ts
+++ wsrc-app/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { RaceResultsComponent } from './race-results/race-results.component';
 import { RaceDetailsComponent } from './race-details/race-details.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
+import { ResultDetailsComponent } from './result-details/result-details.component';
+import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [
@@ -38,5 +41,20 @@ export const routes: Routes = [
         path: 'profile',
         component: UserProfileComponent,
         title: 'Profile'
+    },
+    { 
+        path: 'withdraw',
+        component: WithdrawComponent,
+        title: 'Withdraw'
+    },
+    { 
+        path: 'result-details',
+        component: ResultDetailsComponent,
+        title: 'Result Details'
+    },
+    { 
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login'
     }
 ];

9bea020e9735eb7195f1627f4eece86dd117704c
Lizarlfos1 <blizzardoctopus@gmail.com>
2024-12-09 14:44:15 +1100

    image added with track name

--- wsrc-app/tailwind.config.js
+++ wsrc-app/tailwind.config.js
@@ -13,7 +13,9 @@ module.exports = {
         cWhite: '#F2F2F3',   // Off White
         cBlue: '#006EFF', // Strong Blue
         cGrey: '#B7B7B7',       // Grey
-        cDarkGrey: '#C1CDC4'
+        cDarkGrey: '#C1CDC4',
+        cBorderGrey: '#4A4B4C'
+
        },
       height: {
         '144': '32rem', // Custom height class

7ee077eb804692dca653e2ee33b45c11ab693160
Lizarlfos1 <blizzardoctopus@gmail.com>
2024-12-10 12:00:42 +1100

    mat icons added

--- wsrc-app/src/app/races/races.component.ts
+++ wsrc-app/src/app/races/races.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-races',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './races.component.html',
   styleUrl: './races.component.css'
 })

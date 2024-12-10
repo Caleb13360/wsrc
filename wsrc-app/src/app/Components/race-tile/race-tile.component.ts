929c4531c7a301af04d78d7c8ca46ffadf48f034
Caleb Smith <caleb13360@gmail.com>
2024-12-10 23:10:47 +1100

    added race tile component

---
+++ wsrc-app/src/app/Components/race-tile/race-tile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-race-tile',
+  imports: [],
+  templateUrl: './race-tile.component.html',
+  styleUrl: './race-tile.component.css'
+})
+export class RaceTileComponent {
+
+}

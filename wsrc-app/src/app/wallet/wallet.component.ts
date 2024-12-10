bdefe70b320a38d456c9fb02187821a01faac7b7
Caleb Smith <caleb13360@gmail.com>
2024-12-11 00:31:13 +1100

    added links between pages througout app

--- wsrc-app/src/app/wallet/wallet.component.ts
+++ wsrc-app/src/app/wallet/wallet.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-wallet',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './wallet.component.html',
   styleUrl: './wallet.component.css'
 })

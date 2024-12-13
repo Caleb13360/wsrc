24b49e4f8b672e5ae3f5cd9679dc5e6f5cb198fc
Lizarlfos1 <blizzardoctopus@gmail.com>
2024-12-13 19:44:43 +1100

    Double RP Ui page made

--- wsrc-app/src/app/double-rp/double-rp.component.ts
+++ wsrc-app/src/app/double-rp/double-rp.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-double-rp',
-  imports: [],
+  imports: [MatIconModule, RouterLink, CommonModule],
   templateUrl: './double-rp.component.html',
   styleUrl: './double-rp.component.css'
 })
 export class DoubleRPComponent {
-
+  races = [{
+    date: '2023-10-01',
+    start_time: '10:00 AM',
+    car: 'Ferrari',
+    track: 'Silverstone',
+    prize_pool: '$10,000',
+    racers: '18/20'
+  }];
 }

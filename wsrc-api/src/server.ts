58fd16cdc4fbbb26bef3a6befbe40586cc4bff0e
Caleb Smith <caleb13360@gmail.com>
2024-12-14 00:13:23 +1100

    moves events into a seperate folder

--- wsrc-api/src/server.ts
+++ wsrc-api/src/server.ts
@@ -7,6 +7,12 @@ export const app = express();
 export let httpServer: ReturnType<typeof http.createServer>;
 
 export const Main = () => {
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        next();
+    });
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     app.use('/', routes);
--- wsrc-app/src/app/home/home.component.ts
+++ wsrc-app/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import type {Race} from '../../../../models/race.d.ts';
 import { RouterLink } from '@angular/router';
 
@@ -11,16 +11,25 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
-  // httpClient:HttpClient  = inject(HttpClient);
-  // latestRace: Race | undefined;
+  constructor(private httpClient: HttpClient) {}
+
+  latestRace: Race | undefined;
+  totalPrizeAmount: number = 0;
   remainingTime: string = '';
   ngOnInit(): void {
-    // this.getLatestRace();
+    this.getLatestRace();
+    this.getTotalMoney();
   }
-  // getLatestRace() {
-  //   this.httpClient.get('http://localhost:3000/race/latest').subscribe((data) => {
-  //     this.latestRace = data as Race;
-  //   });
-  // }
+  getLatestRace() {
+    this.httpClient.get('http://localhost:3000/race/latest').subscribe((data) => {
+      this.latestRace = data as Race;
+    });
+  }
+
+  getTotalMoney() {
+    this.httpClient.get<{ totalPrizeAmount: number }>('http://localhost:3000/stats/total-prize-amount').subscribe((data) => {
+      this.totalPrizeAmount = data.totalPrizeAmount;
+    });
+  }
 }
  

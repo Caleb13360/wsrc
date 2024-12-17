ceb14d72fc196c16d8344e674673eaa870b21a34
Lizarlfos1 <blizzardoctopus@gmail.com>
2024-12-17 20:26:51 +1100

    responsive table added to races

--- wsrc-app/src/app/races/races.component.ts
+++ wsrc-app/src/app/races/races.component.ts
@@ -10,12 +10,30 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './races.component.css'
 })
 export class RacesComponent {
-  races = [{
-    date: '2023-10-01',
-    start_time: '10:00 AM',
-    car: 'Ferrari',
-    track: 'Silverstone',
-    prize_pool: '$10,000',
-    racers: '18/20'
-  }];
+  races = [
+    {
+      date: '2024-10-01',
+      start_time: '10:00 AM',
+      car: 'Ferrari SF90',
+      track: 'Silverstone',
+      prize_pool: '$10,000',
+      racers: '18/20'
+    },
+    {
+      date: '2024-10-05',
+      start_time: '2:00 PM',
+      car: 'Toyota GR86',
+      track: 'Spa-Francorchamps',
+      prize_pool: '$5,000',
+      racers: '15/20'
+    },
+    {
+      date: '2024-10-10',
+      start_time: '6:30 PM',
+      car: 'Porsche 911 GT3',
+      track: 'Nürburgring',
+      prize_pool: '$7,500',
+      racers: '20/20'
+    }
+  ];
 }

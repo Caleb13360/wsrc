c3be6ebff3da049883ded0a6f9c28b9b22d97f11
Lizarlfos1 <blizzardoctopus@gmail.com>
2024-12-18 17:22:54 +1100

    race results responsive

--- wsrc-app/src/app/race-results/race-results.component.ts
+++ wsrc-app/src/app/race-results/race-results.component.ts
@@ -16,5 +16,21 @@ export class RaceResultsComponent {
     track: 'Silverstone',
     prize_pool: '$10,000',
     racers: '18/20'
+  },
+{
+    date: '2023-10-01',
+    start_time: '10:00 AM',
+    car: 'Ferrari',
+    track: 'Silverstone',
+    prize_pool: '$10,000',
+    racers: '18/20'
+  },
+{
+    date: '2023-10-01',
+    start_time: '10:00 AM',
+    car: 'Ferrari',
+    track: 'Silverstone',
+    prize_pool: '$10,000',
+    racers: '18/20'
   }];
 }

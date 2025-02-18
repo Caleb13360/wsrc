0669a67ff50caf4c23b6a5a7993d71b819cd633e
Caleb Smith <caleb13360@gmail.com>
2025-02-18 23:13:30 +1100

    added live countdown to top bar with live checker

--- wsrc-app/src/app/app.component.ts
+++ wsrc-app/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { OnInit, Component, HostListener } from '@angular/core';
 import { RouterLink, RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { Race } from '@models/race';
 import { filter } from 'rxjs/operators';
+import { ApiService } from '../services/api';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,21 @@ import { filter } from 'rxjs/operators';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private apiService: ApiService) {}
   title = 'wsrc-app';
      menuVisible = false;
       isHeaderHidden = false; // Tracks if the header is hidden
   lastScrollY = 0; // Tracks the last scroll position
   showFooter: boolean = true;
+  countdown = {
+    days: '?',
+    hours: '?',
+    minutes: '?',
+    seconds: '?'
+  };
+  race!: Race;
+  intervalId: any;
+  isLive: boolean = false;
 
   toggleMenu() {
     this.menuVisible = !this.menuVisible;
@@ -48,7 +59,32 @@ export class AppComponent implements OnInit {
 
     this.lastScrollY = currentScrollY;
   }
+
+  private updateCountdown(): void {
+    if(!this.race){return;}
+    const now = new Date().getTime();
+    const launchTime = new Date(this.race.launch_time).getTime();
+    const distance = launchTime - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    this.countdown = {
+      days: days.toString(),
+      hours: hours.toString().padStart(2, '0'),
+      minutes: minutes.toString().padStart(2, '0'),
+      seconds: seconds.toString().padStart(2, '0')
+    };
+  }
+
   ngOnInit() {
+    this.apiService.isLiveStreaming().subscribe((data)=> {this.isLive=!data.includes("offline")});
+    this.apiService.getLatestRaces(3).subscribe((data)=> {this.race=data.races[0]});
+    this.intervalId = setInterval(() => {
+      this.updateCountdown();
+    }, 1000);
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
